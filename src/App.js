e8009fe86f362a0a54db7d63bccf4da67f0285e4
Anh Tu <trananhtu0103@gmail.com>
2021-06-06 15:02:10 +0700

    implement code that pulls in our shop data from firestore and stores it into the shop reducer

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ class  App extends React.Component {
             ...snapShot.data()
           })
         })
-      } else {
-        setCurrentUser(currentUser)
       }
+
+      setCurrentUser(currentUser)
     })
   }
 
@@ -58,11 +58,11 @@ class  App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,12 +2,54 @@ import React from "react"
 import { Route } from "react-router-dom"
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component.jsx"
 import CollectionPage from "../collection/collection.component.jsx"
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
+import {connect} from 'react-redux'
+import {updateCollections} from '../../redux/shop/shop.actions'
+import WithSpinner from "../../components/with-spinner/with-spinner.component.jsx"
 
-const ShopPage = ({match}) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-)
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
-export default ShopPage
+class ShopPage extends React.Component {
+  state = {
+    isLoading: true
+  }
+
+  unsubscribeFromSnapShot = null
+
+  componentDidMount() {
+    const {updateCollections} = this.props
+    const collectionRef = firestore.collection('collections')
+
+    this.unsubscribeFromSnapShot = collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      updateCollections(collectionsMap)
+      this.setState({isLoading: false})
+    })
+  }
+
+  render() {
+    const {match} = this.props
+    const {isLoading} = this.state
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => <CollectionsOverviewWithSpinner isLoading={isLoading} {...props} />}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+          render={props => <CollectionPageWithSpinner isLoading={isLoading} {...props} />}
+        />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage)
